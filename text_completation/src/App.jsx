6b55cc2bba451cbb7a5cf0ce98d850b6d63a830e
AnirbanDutta-code <anirbandutt0@gmail.com>
2026-01-17 13:56:14 +0530

    fixes

--- text_completation/src/App.jsx
+++ text_completation/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
     setLoading(true)
     try {
-      const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:5000'
+      const apiUrl = 'http://localhost:5000'
       const response = await fetch(`${apiUrl}/generate`, {
         method: 'POST',
         headers: {
